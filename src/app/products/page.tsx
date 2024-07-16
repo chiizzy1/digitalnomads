cd2a76e40c0f51577719c4fe9b012ce1f2e1b34a
Chinwuba Israel <chiizzy5@gmail.com>
2024-07-16 12:19:18 +0100

    a

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,97 +1,11 @@
-import Hero from "@/components/Hero";
-import GetStarted from "@/components/home/GetStarted";
-import Paragraph from "@/components/ui/Paragraph";
-import SmallHeading from "@/components/ui/SmallHeading";
-import TitleIntro from "@/components/ui/TitleIntro";
-import { getAllProducts } from "@/lib/products";
-import { Products } from "@/types";
-import { ChevronRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { FC } from 'react'
 
-export default async function ProductsPage() {
-  const { data } = await getAllProducts();
-  const allProducts = data?.data as Products[];
-  return (
-    <main className="relative min-h-screen">
-      <Hero title="Products" />
-
-      <section className="sm:py-24 py-16 w-full relative z-[1]">
-        <div className="container max-w-6xl mx-auto flex flex-col justify-center items-center w-full">
-          <div className="flex flex-col items-center gap-4 mb-16">
-            <TitleIntro>OUR PRODUCTS</TitleIntro>
-            <SmallHeading size="lg" className="text-center">
-              We Get you Higher Revenues, Not just Higher Ranks
-            </SmallHeading>
-
-            <Paragraph className="text-center leading-loose">
-              Lorem Dolores suscipit esse, fugiat optio consequuntur deserunt
-              velit? Nemo.
-            </Paragraph>
-          </div>
-
-          <div className="flex flex-col gap-16 w-full">
-            {allProducts.map(
-              ({ id, icon, title, description, image, list }, index) => (
-                <div key={id}>
-                  <div className="flex flex-col md:flex-row gap-6">
-                    <div className="flex flex-col items-center md:items-start gap-4 w-full">
-                      <Image
-                        src={icon}
-                        width={100}
-                        height={100}
-                        alt="case study icon"
-                      />
-                      <SmallHeading>{title}</SmallHeading>
-                      <Paragraph className="text-center max-w-[60ch] md:text-start leading-loose">
-                        {description}
-                      </Paragraph>
-                      <ul className="flex justify-center md:justify-start gap-6 bg-gray-400/10 rounded-lg w-full p-4">
-                        {list.map(({ key, value }) => (
-                          <li key={id} className="flex flex-col">
-                            <span className="text-primary font-light block text-[14px] uppercase tracking-wide">
-                              {key}
-                            </span>
-                            <span className="font-semibold text-lg">
-                              {value}
-                            </span>
-                          </li>
-                        ))}
-                      </ul>
-
-                      <Link
-                        href={`/products/${id}`}
-                        className="default-btn w-fit text-base font-thin"
-                      >
-                        <span className="bg-primary rounded-md p-1">
-                          <ChevronRight size={40} />
-                        </span>
-                        View More
-                      </Link>
-                    </div>
-                    <div className="w-full relative">
-                      <div className="case_gradient hidden sm:block opacity-60 w-[450px] absolute right-0 top-0 h-[450px] rounded-full -z-[1]" />
-                      <Image
-                        src={image}
-                        width={1000}
-                        height={1000}
-                        alt="our case icon"
-                      />
-                    </div>
-                  </div>
-                  {index === allProducts.length - 1 ? (
-                    ""
-                  ) : (
-                    <div className="shrink-0 my-6 w-full h-[.5px] bg-[#e3e2ea]" />
-                  )}
-                </div>
-              )
-            )}
-          </div>
-        </div>
-      </section>
-
-      <GetStarted />
-    </main>
-  );
+interface pageProps {
+  
 }
+
+const page: FC<pageProps> = ({}) => {
+  return <div>page</div>
+}
+
+export default page
--- src/app/services/[id]/page.tsx
+++ src/app/services/[id]/page.tsx
@@ -1,92 +1,11 @@
-import Hero from "@/components/Hero";
-import Paragraph from "@/components/ui/Paragraph";
-import SmallHeading from "@/components/ui/SmallHeading";
-import { serviceDetails, servicesList } from "@/constants";
-import { ArrowRight, ChevronRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { getAllServices, getServiceByid } from "@/lib/services";
-import { Service } from "@/types";
-import GetStarted from "@/components/home/GetStarted";
+import { FC } from 'react'
 
-export const dynamicParams = false;
-
-export async function generateStaticParams() {
-  const { data } = await getAllServices();
-  const allServices = data?.data as Service[];
-  return allServices.map((service) => ({ id: service.id }));
+interface pageProps {
+  
 }
 
-export default async function ServicePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const data = await getServiceByid(params.id);
-  const service = data?.data as Service;
-
-  const allServicesData = await getAllServices();
-  const allServices = allServicesData.data.data as Service[];
-
-  const { id, content, contentImage } = service;
-
-  return (
-    <main className="min-h-screen">
-      <Hero title="Service Details" />
-      <div className="sm:py-24 py-16 w-full">
-        <div className="container max-w-6xl mx-auto">
-          <div className="grid grid-cols-12 gap-6 w-full">
-            <div className="col-span-12 md:col-span-8">
-              <Image
-                src={contentImage}
-                width={1000}
-                height={1000}
-                alt="service image"
-              />
-              <div className="pt-8 flex flex-col gap-4">
-                {content.map(({ title, details, list }, index) => (
-                  <div className="flex flex-col gap-4" key={title}>
-                    <SmallHeading>{title}</SmallHeading>
-                    <Paragraph className="leading-loose">{details}</Paragraph>
-                    <ul className="flex flex-col list-disc pl-4 list-inside">
-                      {list.map(({ li }) => (
-                        <li key={li} className="leading-[1.8]">
-                          <span className="leading-loose text-base sm:text-[15px] text-black/70 tracking-tight">
-                            {li}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="col-span-12 md:col-span-4">
-              <div className="p-8 border rounded-lg bg-gray-100 flex flex-col gap-2">
-                <SmallHeading> Services List</SmallHeading>
-                <hr />
-                <ul>
-                  {allServices.map(({ id, title }) => (
-                    <li
-                      key={id}
-                      className="bg-white rounded-lg my-3 flex items-center transition-colors duration-500 ease-in-out hover:bg-primary/90 hover:text-white"
-                    >
-                      <Link
-                        className="flex items-center p-2 px-4 justify-between w-full"
-                        href={`/services/${id}`}
-                      >
-                        <span>{title}</span>
-                        <ArrowRight size={16} strokeWidth={1.25} />
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <GetStarted />
-    </main>
-  );
+const page: FC<pageProps> = ({}) => {
+  return <div>page</div>
 }
+
+export default page
